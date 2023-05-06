fix errors - text encoder: handle pad token when making BOW - vid encoder: divide by true lens when global average pooling - eval_q2m: fix np argsort in descending order - evaluation: logits_lat, logits_con min-max norm on global batch

evaluation.py
@@ -31,14 +31,14 @@ def _get_gt(video_ids: List[str], caption_ids: List[str]) -> Tuple[List[List[int
 
 
 def _eval_q2m(scores: np.ndarray, gt: List[List[int]]) -> Tuple[Tuple[float, float, float, float, float], float]:
-    pred = np.argsort(scores, axis=1)[::-1]
+    pred = np.argsort(scores, axis=1)[:, ::-1]
 
     ranks = []
     for pred_indices, gt_indices in zip(pred, gt):
         rank = min(np.argwhere(pred_indices == gt_idx)[0][0] for gt_idx in gt_indices) + 1
         ranks.append(rank)
 
-    ranks = np.stack(ranks) # (64, )
+    ranks = np.stack(ranks)
 
     r1 = (np.sum(ranks <= 1) / scores.shape[1] * 100)
     r5 = (np.sum(ranks <= 5) / scores.shape[1] * 100)
@@ -46,12 +46,13 @@ def _eval_q2m(scores: np.ndarray, gt: List[List[int]]) -> Tuple[Tuple[float, flo
     med_r = np.median(ranks)
     mean_r = np.mean(ranks)
 
-    r_sum = r1 + r5 + r10 + med_r + mean_r
+    sum_r = r1 + r5 + r10
 
-    return (r1, r5, r10, med_r, mean_r), r_sum
+    return (r1, r5, r10, sum_r), med_r, mean_r
 
 
 # batch_size == -1 for global batch size
+@torch.no_grad()
 def evaluate(model: Union[HybridDualEncoding, LatentDualEncoding], video_bundle: VideoBundle, cap_bundle: CaptionBundle, batch_size=-1):
     was_training = model.training
 
@@ -70,30 +71,44 @@ def evaluate(model: Union[HybridDualEncoding, LatentDualEncoding], video_bundle:
     iter_vid = math.ceil(len_vid / bz_vid)
     iter_cap = math.ceil(len_cap / bz_cap)
 
-    global_logits_list = []
+    global_logits_lat_list = []
+    global_logits_con_list = []
     for i in range(iter_vid):
-        logits_list = []
+        logits_lat_list = []
+        logits_con_list = []
+        vid_ids_batch = vid_ids[i*bz_vid:(i+1)*bz_vid]
+
         for j in range(iter_cap):
-            vid_ids_batch = vid_ids[i*bz_vid:(i+1)*bz_vid]
             cap_ids_batch = cap_ids[j*bz_cap:(j+1)*bz_cap]
 
-            vid_batch, vid_batch_lens = video_bundle.to_input_tensor_batch(vid_ids_batch, device=model.device)
-            cap_batch, cap_batch_lens = cap_bundle.to_input_tensor_batch(cap_ids_batch, device=model.device)
+            vid_batch, vid_batch_lens = video_bundle.to_input_tensor_batch(vid_ids_batch, device=model.device, desc=False)
+            cap_batch, cap_batch_lens = cap_bundle.to_input_tensor_batch(cap_ids_batch, device=model.device, desc=False)
 
-            v2t_gt_indices, t2v_gt_indices = _get_gt(vid_ids_batch, cap_ids_batch)
+            (logits_lat_b, _), (logits_con_b, _) = model.forward_sep(vid_batch, vid_batch_lens, cap_batch, cap_batch_lens)
 
-            with torch.no_grad():
-                logits_b, _ = model.forward(vid_batch, vid_batch_lens, cap_batch, cap_batch_lens)
-                logits_b = logits_b.detach().cpu().numpy()
+            logits_lat_list.append(logits_lat_b)
+            logits_con_list.append(logits_con_b)
 
-                logits_list.append(logits_b)
+        logits_lat_row = torch.concat(logits_lat_list, dim=1)
+        logits_con_row = torch.concat(logits_con_list, dim=1)
 
-        logits_row = np.concatenate(logits_list, axis=1)
-        global_logits_list.append(logits_row)
+        global_logits_lat_list.append(logits_lat_row)
+        global_logits_con_list.append(logits_con_row)
 
-    global_logits = np.concatenate(global_logits_list)  # (N, 20*N)
+    global_logits_lat = torch.concat(global_logits_lat_list)  # (N, 20*N)
+    global_logits_con = torch.concat(global_logits_con_list)  # (N, 20*N)
+
+    global_logits_lat = utils.min_max_normalize(global_logits_lat)
+    global_logits_con = utils.min_max_normalize(global_logits_con)
+
+
+    global_logits = model.alpha * global_logits_lat + (1 - model.alpha) * global_logits_con
+    global_logits = global_logits.detach().cpu().numpy()
+
 
     assert global_logits.shape == (len_vid, len_cap)
+
+    v2t_gt_indices, t2v_gt_indices = _get_gt(vid_ids, cap_ids)
 
     v2t_metrics = _eval_q2m(global_logits, v2t_gt_indices)
     t2v_metrics = _eval_q2m(global_logits.T, t2v_gt_indices)